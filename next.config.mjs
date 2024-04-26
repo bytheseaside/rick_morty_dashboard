4ea813a2de9edb217a09a6310c236fd9777b0750
Brisa Rojas <rojaasbrisa@gmail.com>
2024-04-26 19:48:42 -0300

    try again

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compress: false,
-  swcMinify: false,
+  optimization.minimize: false,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'rickandmortyapi.com',
-        // port: '',
-        // pathname: '/account123/**',
       },
     ],
   },
