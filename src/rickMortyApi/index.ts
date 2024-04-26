ccb31095e5c251e6f4b72359ce24afa15cc9a52b
Brisa Rojas <rojaasbrisa@gmail.com>
2024-04-25 23:35:04 -0300

    add response schema

--- src/rickMortyApi/index.ts
+++ src/rickMortyApi/index.ts
@@ -1,4 +1,5 @@
 import Character from "./character";
 import Episode from "./episode";
+import Response from "./response";
 
-export type { Character, Episode };
+export type { Character, Episode, Response };
---
+++ src/rickMortyApi/response.ts
@@ -0,0 +1,11 @@
+type Response<T ='unknown'> = {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  };
+  results: T[];
+}; 
+
+export default Response;
