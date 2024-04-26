f52037df459d67c6497bf57996084ba84cf8903a
Brisa Rojas <rojaasbrisa@gmail.com>
2024-04-26 19:58:19 -0300

    delete comments

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'rickandmortyapi.com',
-        // port: '',
-        // pathname: '/account123/**',
       },
     ],
   },
